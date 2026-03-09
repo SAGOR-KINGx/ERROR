fc972a6835b5dbcbfa12cb8ac49a9b5620b5abf3
JAHIDULLX6 <bid6074@gmail.com>
2026-03-09 15:15:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const { spawn } = require("child_process");
+const log = require("./logger/log.js");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+				res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+				console.log(`✅ Server running at http://localhost:${PORT}`);
+});
+
+function startProject() {
+				const child = spawn("node", ["Goat.js"], {
+								cwd: __dirname,
+								stdio: "inherit",
+								shell: true
+				});
+
+				child.on("close", (code) => {
+								if (code === 2) {
+												log.info("Restarting Project...");
+												startProject();
+								}
+				});
+}
+
+startProject();
